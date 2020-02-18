bcf85a0c8642b77894acc101ff9b5b77befc7b51
Daniel Andrew <dwandrew@hotmail.com>
2020-02-18 16:33:03 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,13 +23,9 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  hash.each_value{value += 1}
-  
-end 
-
-
-#hash.each_value{
-#if hash[key]
- #  hash[key] += 1
- #else
-#   hash[key] = 1;
+  if hash[key]
+ hash[key]+= 1
+ else
+ hash[key] = 1;
+ end
+ end
